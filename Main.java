391b0a7495085e1ba4456650bb773c7bdd6a9882
Scribbles <mr.scribbles.alt@gmail.com>
2024-09-24 20:38:14 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      /*VerticalPlatformer p = new VerticalPlatformer();
+      p.main();*/
+      Platformer.main(args);
+    }
+  }
